cd9ae75e9181b02160d6616d6b2912ba5284e7d4
Vjeko <vjeko.pavicin@gmail.com>
2026-05-05 15:53:53 +0200

    added toolbar icon

--- deskmat/deskmatApp.swift
+++ deskmat/deskmatApp.swift
@@ -11,13 +11,44 @@ import CoreData
 @main
 struct deskmatApp: App {
     let persistenceController = PersistenceController.shared
-
+ 
     var body: some Scene {
-        WindowGroup {
+        WindowGroup(id: "main") {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .onAppear {
+                    NSApp.windows.first(where: {
+                        !$0.className.contains("StatusBar")
+                    }).map { window in
+                        // Remove minimize and fullscreen buttons
+                        window.styleMask.remove(.miniaturizable)
+                        // Hide instead of close
+                        window.isReleasedWhenClosed = false
+                    }
+                }
         }
-        .defaultSize(width: 300, height: 200)
+        .handlesExternalEvents(matching: ["main"])
+        .defaultSize(width: 800, height: 600)
         .windowResizability(.contentMinSize)
+ 
+        MenuBarExtra("Deskmat", systemImage: "photo.on.rectangle") {
+            MenuBarButtons()
+        }
     }
 }
+ 
+struct MenuBarButtons: View {
+    var body: some View {
+        Button("Open Deskmat") {
+            if let url = URL(string: "deskmat://main") {
+                NSWorkspace.shared.open(url)
+            }
+            NSApp.activate(ignoringOtherApps: true)
+        }
+        Divider()
+        Button("Quit") {
+            NSApp.terminate(nil)
+        }
+    }
+}
+
